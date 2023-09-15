8ce202088f8345cf030ada6eaa04128690824b5e
DAdawit <58243647+DAdawit@users.noreply.github.com>
2023-09-15 10:48:12 +0300

    rest of crud functions completed on tasks

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    use HttpResponses;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return TaskResource::collection(
+            Task::where('user_id',Auth::user()->id)->paginate(12)
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TaskRequest $request)
+    {
+        $request->validated($request->all());
+        $task = Task::create([
+           'user_id'=>Auth::user()->id,
+           'name'=>$request["name"],
+           'description'=>$request["description"],
+           'priority'=>$request["priority"]
+        ]);
+        return new TaskResource($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return $this->isNotAuthorized($task) ? $this->isNotAuthorized($task) :  new TaskResource($task);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+
+        if(Auth::user()->id !== $task->user_id){
+            return $this->error('','you are not allowed to access this data',403);
+        }else{
+            $task->update($request->all());
+            return new TaskResource($task);
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        return $this->isNotAuthorized($task) ? $this->isNotAuthorized($task) : $task->delete();
+
+//        if(Auth::user()->id !== $task->user_id){
+//            return $this->error('','you are not allowed to access this data',403);
+//        }else{
+//            $task->delete();
+//            return response()->json('',204,[]);
+//        }
+    }
+
+    private function isNotAuthorized($task){
+        if(Auth::user()->id !== $task->user_id){
+            return $this->error('','you are not allowed to access this data',403);
+        }
+    }
+}
